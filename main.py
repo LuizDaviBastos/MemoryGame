8c70de6daaa7f90e9497ce69c64ed3d060be9056
Luiz Davi Bastos <davi@easycomtec.com>
2021-02-27 05:33:17 -0300

    created method to show matriz in grid

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ list1 = [1, 2, 3, 4]
 list2 = [5, 6, 7, 8]
 globalPositions = []
 
+
 def getMatriz(matriz: list = []):
     mainMatriz = [[], [], [], []]
     position = 0
     for index in range(4):
         position = getPosition()
         if mainMatriz[position].__len__() <= 0:
-            mainMatriz[position] = Random.sample((list1 if index <= 1 else list2), 4)
+            mainMatriz[position] = Random.sample(
+                (list1 if index <= 1 else list2), 4)
             continue
     return mainMatriz
 
+
 def getPosition():
     position = 0
-    countToBreak = 0
     positions = [0, 1, 2, 3]
     for p in globalPositions:
         positions.remove(p)
@@ -26,5 +28,15 @@ def getPosition():
     return position
 
 
+def showMatriz():
+    matrizString = ""
+    for row in mainMatriz:
+        for column in row:
+            breaking = ("\n" if row.index(column) >= 3 else "")
+            matrizString += f"{column} {breaking}"
+    print(matrizString)
+    pass
+
+
 mainMatriz = getMatriz()
-print(mainMatriz)
+showMatriz()
